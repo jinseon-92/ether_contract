bd625585e151582014354b58fd5bf9aa26a201fa
js <lper11@naver.com>
2021-06-17 02:00:42 +0900

    constructor & destroy 추가

--- sc/Faucet.sol
+++ sc/Faucet.sol
@@ -1,6 +1,19 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.22;
 
 contract Faucet {
+
+    address owner;
+
+    // Faucet 컨트랙트 초기화: owner 설정
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    function destroy() public {
+        require(msg.sender == owner);
+        selfdestruct(owner);
+    }
+
     function withdraw(uint withdraw_amount) public {
         require(withdraw_amount <= 0.1 ether);
         msg.sender.transfer(withdraw_amount);
