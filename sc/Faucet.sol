9fcf7d86f6210bcc8935965a499c46a08e9a35b1
js <lper11@naver.com>
2021-06-16 23:00:58 +0900

    ether변환

--- sc/Faucet.sol
+++ sc/Faucet.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.19;
 
 contract Faucet {
     function withdraw(uint withdraw_amount) public {
-        require(withdraw_amount <= 100000000000000000);
+        require(withdraw_amount <= 0.1 ether);
         msg.sender.transfer(withdraw_amount);
     }
     
